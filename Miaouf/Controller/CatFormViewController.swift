e8b4e1ff1e30cd7de418cb8d20186f5887dbdc62
Sporard <pierresabard@gmail.com>
2023-06-22 23:41:08 +0200

    fix: add observer for cat controller

--- Miaouf/Controller/CatFormViewController.swift
+++ Miaouf/Controller/CatFormViewController.swift
@@ -11,7 +11,8 @@ class CatFormViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardAppear(_:)), name: UIViewController.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDisappear(_:)), name: UIViewController.keyboardWillHideNotification, object: nil)
         // Do any additional setup after loading the view.
     }
     
