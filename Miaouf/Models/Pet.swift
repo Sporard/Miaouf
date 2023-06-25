abe8cef99b10ea2d3e2d795e62fa234b582af3fa
Sporard <pierresabard@gmail.com>
2023-06-25 18:23:44 +0200

    chore(extension): use extension for model

--- Miaouf/Models/Pet.swift
+++ Miaouf/Models/Pet.swift
@@ -7,34 +7,37 @@
 
 import Foundation
 struct Pet {
-  var name: String?
-  var hasMajority: Bool
-  var phone: String?
-  var race: String?
-  var gender: Gender
-    var status: Status {
-        if name == nil || name == "" {
-          return .rejected("Vous n'avez pas indiqué votre nom !")
-        }
-        if phone == nil || phone == "" {
-          return .rejected("Vous n'avez pas indiqué votre téléphone !")
-        }
-        if race == nil || race == "" {
-          return .rejected("Quel est votre race ?")
-        }
-        if !hasMajority {
-          return .rejected("Les mineurs ne sont pas admis.")
-        }
-
-        return .accepted
+    var name: String?
+    var hasMajority: Bool
+    var phone: String?
+    var race: String?
+    var gender: Gender
+    
+    enum Gender {
+        case male
+        case female
     }
+}
 
-  enum Gender {
-    case male
-    case female
-  }
+extension Pet {
     enum Status {
         case accepted
         case rejected(String)
+    }
+    var status: Status {
+        guard let name = name, name.isEmpty == false  else {
+            return .rejected("Vous n'avez pas indiqué votre nom !")
+        }
+        guard let phone = phone, phone.isEmpty == false else {
+            return .rejected("Vous n'avez pas indiqué votre téléphone !")
+        }
+        guard let race = race, race.isEmpty == false else {
+            return .rejected("Quel est votre race ?")
+        }
+        guard hasMajority else{
+            return .rejected("Les mineurs ne sont pas admis.")
+        }
+        
+        return .accepted
     }
 }
