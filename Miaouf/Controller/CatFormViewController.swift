13c0c62a08b0aee068d8be465ad7e80585761a91
Sporard <pierresabard@gmail.com>
2023-06-25 18:41:18 +0200

    feat: add feature for cat form

--- Miaouf/Controller/CatFormViewController.swift
+++ Miaouf/Controller/CatFormViewController.swift
@@ -8,24 +8,8 @@
 import UIKit
 
 class CatFormViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardAppear(_:)), name: UIViewController.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDisappear(_:)), name: UIViewController.keyboardWillHideNotification, object: nil)
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    // MARK: Outlet
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var genderSegmented: UISegmentedControl!
     @IBOutlet weak var phoneTextField: UITextField!
@@ -34,10 +18,16 @@ class CatFormViewController: UIViewController {
     
     
     
-    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
-        nameTextField.resignFirstResponder()
-        phoneTextField.resignFirstResponder()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardAppear(_:)), name: UIViewController.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDisappear(_:)), name: UIViewController.keyboardWillHideNotification, object: nil)
+        // Do any additional setup after loading the view.
     }
+    
+    
+    
+
     @objc func keyboardAppear(_ notification: Notification){
         guard let frame = notification.userInfo? [UIViewController.keyboardFrameEndUserInfoKey] as? NSValue else { return }
         let keyboardFrame = frame.cgRectValue
@@ -52,26 +42,11 @@ class CatFormViewController: UIViewController {
         }
     }
 
-    @IBAction func validateForm(_ sender: Any) {
-        self.createCatPet()
-    }
     
-    
-    private func createCatPet(){
-        let name = nameTextField.text
-        let phone = phoneTextField.text
-        let hasMajority = hasMajoritySwitch.isOn
-        let genderSelectedIndex = genderSegmented.selectedSegmentIndex
-        let genderChosen: Pet.Gender = genderSelectedIndex == 0 ? Pet.Gender.male : Pet.Gender.female
-        
-        let raceIndex = catRacesPickerView.selectedRow(inComponent: 0)
-        let race = dogRaces[raceIndex]
-        
-        
-        let cat = Pet(name: name, hasMajority: hasMajority, phone: phone, race: race, gender: genderChosen)
-        
-    }
 }
+
+
+// MARK: pickerView
 extension CatFormViewController: UIPickerViewDataSource{
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -88,9 +63,52 @@ extension CatFormViewController: UIPickerViewDelegate{
     }
 }
 
+// MARK: Keyboard
 extension CatFormViewController: UITextFieldDelegate{
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        phoneTextField.resignFirstResponder()
+    }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+// MARK: Validation
+extension CatFormViewController {
+    @IBAction func validateForm(_ sender: Any) {
+        let pet = self.createCatPet()
+        self.validatePet(pet)
+    }
+    
+    private func validatePet(_ pet: Pet){
+        switch (pet.status){
+        case .accepted:
+            performSegue(withIdentifier: "segueToSuccess", sender: pet)
+        case .rejected(let reason):
+            self.presentAlert(reason)
+        }
+    }
+    private func presentAlert(_ reason: String) {
+        let alertVC = UIAlertController(title: "Erreur", message: reason, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertVC.addAction(action)
+        present(alertVC, animated: true, completion: nil)
+    }
+    
+    private func createCatPet() -> Pet {
+        let name = nameTextField.text
+        let phone = phoneTextField.text
+        let hasMajority = hasMajoritySwitch.isOn
+        let genderSelectedIndex = genderSegmented.selectedSegmentIndex
+        let genderChosen: Pet.Gender = genderSelectedIndex == 0 ? Pet.Gender.male : Pet.Gender.female
+        
+        let raceIndex = catRacesPickerView.selectedRow(inComponent: 0)
+        let race = dogRaces[raceIndex]
+        
+        
+        return Pet(name: name, hasMajority: hasMajority, phone: phone, race: race, gender: genderChosen)
+        
     }
 }
